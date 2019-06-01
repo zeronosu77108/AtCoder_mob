ca3907cd97c92178420d082fee4089dec165b71b
uniuni76 <e155766@ie.u-ryukyu.ac.jp>
2019-06-01 16:32:28 +0900

    refactor D.cpp

--- ABC126/D.cpp
+++ ABC126/D.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 #define ASIZE 100010
 
-void dfs(const long& i, const long& start, const long& rootW,  const long& n, vector< pair<long,long> > (&ad)[ASIZE], vector<bool>& visited, vector<bool>& ans) {
+void dfs(const int& i, const int& start, const int& rootW,  const int& n, vector< pair<int,bool> > (&ad)[ASIZE], vector<bool>& visited, vector<bool>& ans) {
     if ( visited[i] ) return;
     visited[i] = true;
     ans[i] = rootW%2==0 ? ans[start] : !ans[start];
@@ -19,28 +19,28 @@ void dfs(const long& i, const long& start, const long& rootW,  const long& n, ve
 }
 
 int main() {
-    long n;
+    int n;
     cin >> n;
-    vector < pair<long,long> > ad [ASIZE];
+    vector < pair<int,bool> > ad [ASIZE];
     vector <bool> visited (ASIZE, false);
     vector <bool> ans (ASIZE, false);
 
-    for (long i=1; i<n; i++) {
-        long u,v,w;
+    for (int i=1; i<n; i++) {
+        int u,v,w;
         cin >> u >> v >> w;
         // ad[u][v] = ad[v][u] = w;
-        ad[u].push_back( make_pair(v,w) );
-        ad[v].push_back( make_pair(u,w) );
+        ad[u].push_back( make_pair(v,w%2) );
+        ad[v].push_back( make_pair(u,w%2) );
     }
     
 
-    for (long i=1; i<=n; i++) {
+    for (int i=1; i<=n; i++) {
         if ( visited[i] ) continue;
         ans[i] = true;
         dfs(i, i, 0, n, ad, visited, ans);
     }
 
-    for (long i=1; i<=n; i++) {
+    for (int i=1; i<=n; i++) {
         cout << ans[i] << endl;
     }
     // cout << endl;
